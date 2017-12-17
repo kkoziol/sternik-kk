b0cf3072aa593c15b28bd7e0f6320897da437692
Krzysiek Koziol <kkoziol@gmail.com>
2017-12-17 14:28:41 +0100

    refactoring - kasowanie

--- zad-01/pl/sternik/kk/zadania/Zad01.java
+++
@@ -1,9 +0,0 @@
-package pl.sternik.kk.zadania;
-
-public class Zad01{
-
-    public static void main(String[] args){
-	    System.out.println("Linikja nr 1");
-		System.out.println("Linikja nr 2");
-	}
-}
--- zad-02/zad02.bat
+++
@@ -1,1 +0,0 @@
-jar -cfe zad01.jar pl.sternik.kk.zadania.Zad01 pl/sternik/kk/zadania/Zad01.class
