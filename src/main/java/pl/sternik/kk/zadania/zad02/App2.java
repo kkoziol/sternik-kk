0c7768ffb4a0de31edd91487bbb790e4fa9cf1c7
Krzysiek Koziol <kkoziol@gmail.com>
2017-12-17 14:28:15 +0100

    refactoring poczatek

---
+++ src/main/java/pl/sternik/kk/zadania/zad02/App2.java
@@ -0,0 +1,26 @@
+package pl.sternik.kk.zadania.zad02;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+
+        int[] tabliczka = {22,3,2,4,6,8,9,0};
+
+        for (int i = 0; i < tabliczka.length; i++) {
+            System.out.println(tabliczka[i]);
+        }
+        int j=0;
+        while (j< tabliczka.length){
+            System.out.println(tabliczka[j]);
+            j++;
+        }
+
+    }
+}
