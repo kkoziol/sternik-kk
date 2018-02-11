2b1db31b055c7f82ff1b8c79ec5b7046ee347d94
Krzysiek Koziol <kkoziol@gmail.com>
2018-02-11 15:51:59 +0100

    Zad mockito test

--- src/test/java/pl/sternik/kk/zadania/zadmockito/ZadMockito.java
+++ src/test/java/pl/sternik/kk/zadania/zadmockito/ZadMockito.java
@@ -7,16 +7,28 @@ import org.junit.Test;
 
 public class ZadMockito {
     
-//        UserService userService = mock(UserService.class); 
-//        AuthService authService = new AuthServiceImpl(userService); 
+        UserService userService = mock(UserService.class); 
+//        UserService userService = new UserService();
+        AuthService authService = new AuthService(userService);
      
         @Test 
         public void testBadLogin() { 
-//            when(userService.findByName(”xyz”)).thenReturn(null); 
+            when(userService.findByName("xyz")).thenReturn("null");
+            when(userService.findByName("root")).thenReturn("lipa");
        
-//            boolean authOk = as.checkCredentials(„xyz”, „password”); 
+            boolean authOk = authService.checkCredentials("xyz", "password"); 
      
-//            assertThat(authOk).isFalse();     
-//            verify(userService).findByName(”xyz”); 
+            assertThat(authOk).isFalse();     
+            verify(userService,atLeastOnce()).findByName("xyz");
+            verify(userService,never()).findByName("root");
+            verify(userService).findByName("xyz"); 
+        } 
+        
+        @Test(expected=NullPointerException.class)
+        public void testBadLoginException() { 
+            when(userService.findByName(anyString())).thenThrow(NullPointerException.class);
+      
+            authService.checkCredentials("ZSW", "password"); 
+     
         } 
 }
