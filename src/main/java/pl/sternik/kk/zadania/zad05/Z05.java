90f24ee3d01a61b0c28f3a9176fbf8e8ec581379
Krzysiek Koziol <kkoziol@gmail.com>
2018-02-11 12:51:41 +0100

    Zad 5 testy poprawka

--- src/main/java/pl/sternik/kk/zadania/zad05/Z05.java
+++ src/main/java/pl/sternik/kk/zadania/zad05/Z05.java
@@ -9,38 +9,41 @@ public class Z05 {
         System.out.println(tmp);
 
         tmp = zrobPetleForEach(tablica);
-//        System.out.println(tmp);
+        System.out.println(tmp);
 
         tmp = zrobPetleWhile(tablica);
-//        System.out.println(tmp);
+        System.out.println(tmp);
         
     }
 
-	static String zrobPetleFor(String[] tablica) {
-		int i;
-		String tmp = "Petla FOR";
+    static String zrobPetleFor(String[] tablica) {
+        int i;
+        StringBuilder sb = new StringBuilder();
+        sb.append("Petla FOR");
         for (i = 0; i <= tablica.length - 1; i++) {
-            tmp = tmp + "\n" +tablica[i];
+            sb.append("\n"); 
+            sb.append(tablica[i]);
         }
-        return tmp;
-	}
-	static String zrobPetleWhile(String[] tablica) {
-		int i;
+        return sb.toString();
+    }
+    static String zrobPetleWhile(String[] tablica) {
+        int i;
+        StringBuilder sb = new StringBuilder();
+        sb.append("Petla While");
         i = 0;
         while (i <= tablica.length - 1) {
-            System.out.println(tablica[i]);
+            sb.append("\n");
+            sb.append(tablica[i]);
             i++;
         }     
-        return "";
-	}	
-	static String zrobPetleForEach(String[] tablica) {
-		String tmp ="";
-		//brzydka metoda sklejania Stringa.
-		tmp = tmp + "Petla FOREach";
-		for (String string : tablica) {
+        return sb.toString();
+    }   
+    static String zrobPetleForEach(String[] tablica) {
+        String tmp ="";
+        tmp = tmp + "Petla FOREach";
+        for (String string : tablica) {
             tmp = tmp +"\n" + string;
         }
-		System.out.println(tmp);
-		return tmp;
-	}
+        return tmp;
+    }
 }
