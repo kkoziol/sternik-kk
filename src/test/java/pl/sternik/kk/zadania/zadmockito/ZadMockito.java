3fff5eac66a24a74a3fb4e104ee2d0eb864af525
Krzysiek Koziol <kkoziol@gmail.com>
2018-02-11 14:00:54 +0100

    zad mockito

---
+++ src/test/java/pl/sternik/kk/zadania/zadmockito/ZadMockito.java
@@ -0,0 +1,22 @@
+package pl.sternik.kk.zadania.zadmockito;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*; 
+
+import org.junit.Test;
+
+public class ZadMockito {
+    
+//        UserService userService = mock(UserService.class); 
+//        AuthService authService = new AuthServiceImpl(userService); 
+     
+        @Test 
+        public void testBadLogin() { 
+//            when(userService.findByName(”xyz”)).thenReturn(null); 
+       
+//            boolean authOk = as.checkCredentials(„xyz”, „password”); 
+     
+//            assertThat(authOk).isFalse();     
+//            verify(userService).findByName(”xyz”); 
+        } 
+}
